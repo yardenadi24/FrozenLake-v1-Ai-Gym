1f7f6795f8323a9fc9890ca2c99e445076d180e1
yarden <yardenadi24@gmail.com>
2021-11-22 18:05:34 +0200

    Added epsilon greedy plocy function

--- frozen_lake.py
+++ frozen_lake.py
@@ -3,3 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 env = gym.make("FrozenLake8x8-v1")
+
+gamma = 0.95
+num_actions = 4
+num_states = 64
+def eps_greedy_policy(Q,epsilon,s):
+    action_prob = np.ones(num_actions,dtype=float) * epsilon /num_actions
+    greedy_action = np.argmax(Q[s])
+    action_prob[greedy_action] +=(1.0-epsilon)
+    return action_prob
+
